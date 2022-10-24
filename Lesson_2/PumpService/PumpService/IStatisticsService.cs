fa05da980b65c7beabb4099bea7e00d456fa2fcc
[VolAnton] <[_Tocuk@mail.ru]>
2022-10-25 01:45:16 +0300

    Домашнее задание №2

---
+++ Lesson_2/PumpService/PumpService/IStatisticsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpService
+{
+    public interface IStatisticsService
+    {
+        int SuccessTacts { get; set; }
+
+        int ErrorTacts { get; set; }
+
+        int AllTacts { get; set; }
+
+    }
+}
