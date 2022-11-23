f7e7f3d234226170f7689ece991d771f6b427522
VolAnton <Tocuk@tut.by>
2022-11-23 17:19:45 +0300

    Домашнее задание №6

---
+++ Lesson_6/RootService/SampleService/Services/Client/IRootServiceClient.cs
@@ -0,0 +1,12 @@
+﻿using RootServiceNamespace;
+
+namespace SampleService.Services.Client
+{
+    public interface IRootServiceClient
+    {
+        public RootServiceNamespace.RootServiceClient RootServiceClient { get; }
+
+        public Task<IEnumerable<WeatherForecast>> Get();
+
+    }
+}
